bdc1c7cb0d7be223aa9ff6d92ea32e4480f50e7c
AdanSmithuwu <adancalderonramirez70@gmail.com>
2026-03-06 22:01:22 -0500

    Documentacion

---
+++ src/main/java/com/calderon/comercialsvalerio/presentation/PingController.java
@@ -0,0 +1,13 @@
+package com.calderon.comercialsvalerio.presentation;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "Sistema comercial valerio activo";
+    }
+}
