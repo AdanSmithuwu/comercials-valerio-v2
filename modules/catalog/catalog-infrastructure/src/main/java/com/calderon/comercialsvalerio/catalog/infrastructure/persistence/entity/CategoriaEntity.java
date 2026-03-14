5ba11ecba677e1df7e884d3a9f857d6fdfaf5683
AdanSmithuwu <adancalderonramirez70@gmail.com>
2026-03-14 15:46:48 -0500

    Spring boot + enfoque modular

---
+++ modules/catalog/catalog-infrastructure/src/main/java/com/calderon/comercialsvalerio/catalog/infrastructure/persistence/entity/CategoriaEntity.java
@@ -0,0 +1,4 @@
+package com.calderon.comercialsvalerio.catalog.infrastructure.persistence.entity;
+
+public class CategoriaEntity {
+}
---
+++ modules/iam/iam-domain/src/main/java/com/calderon/comercialsvalerio/iam/domain/model/BitacoraLogin.java
@@ -0,0 +1,4 @@
+package com.calderon.comercialsvalerio.iam.domain.model;
+
+public class BitacoraLogin {
+}
